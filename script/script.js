0d00ad5e3e8570882ad296d730f1fe23b3443f16
maiky <maiky.williamfs@gmail.com>
2023-03-31 17:40:06 -0300

    fix

--- script/script.js
+++ script/script.js
@@ -1,84 +1,96 @@
 // const events = require("inquirer/lib/utils/events");
 
 class Futbah {
-  constructor() {
-    this.urlListJogador = 'jogadroes/list_jogador.json';
-    this.urlNoticia = 'jogadroes/list_noticia.json';
-    this.paragrafo = document.getElementById("paragrafo");
-    this.elencoJogadores = document.getElementById("elenco");
-    this.banner = document.getElementById(".banner");
-    this.btnNoticia = "";
-  }
-
-  async run() {
-    await this.noticia();
-    await this.elenco();
-    await this.rankClassificacao();
-    this.clicando();
-    // this.btnNoticia = document.querySelectorAll('.btn_noticia')
-  }
-
-  // Procura no json de noticia, e informa os dados do jogo
-  async noticia() {
-
-    try {
-      const response = await fetch(this.urlNoticia);
-      const json = await response.json();
-
-      var i = 1;
-      json.jogo.map((noticia) => {
-        let header = noticia.titulo;
-        let paragrafo = noticia.noticia1;
-        this.data = noticia.data;
-
-        var divNoticia = document.createElement("div");
-        // var divTexto = document.createElement("div");
-        var img = document.createElement("img");
-        var pData = document.createElement("p");
-        var pParagrafo = document.createElement("p");
-        var h2 = document.createElement("h2");
-        var a = document.createElement("a");
-
-        img.textContent = `images/foto-${i}.jpg`;
-        pData.textContent = this.data;
-        h2.textContent = header;
-        pParagrafo.textContent = paragrafo.substring(0, 250) + "...";
-        a.textContent = `Saiba mais...`;
-
-        // divNoticia.appendChild(divTexto);
-        divNoticia.appendChild(img);
-        divNoticia.appendChild(pData);
-        divNoticia.appendChild(h2);
-        divNoticia.appendChild(pParagrafo);
-        divNoticia.appendChild(a);
-
-        img.setAttribute("src", `images/foto-${i}.jpg`);
-        divNoticia.classList.add("noticia");
-        divNoticia.classList.add("w33");
-        divNoticia.classList.add(this.data);
-        a.classList.add("btn_noticia");
-        // a.setAttribute("href", "noticia-single.html");
-        this.paragrafo.appendChild(divNoticia)
-        a.setAttribute("onclick", "futbah.clicando()");
-        a.classList.add(this.data);
-        i++
-      })
-
-    } catch (error) {
-      // console.log(error)
+    constructor() {
+        this.urlListJogador = 'jogadroes/list_jogador.json';
+        this.urlNoticia = 'jogadroes/list_noticia.json';
+        this.paragrafo = document.getElementById("paragrafo");
+        this.elencoJogadores = document.getElementById("elenco");
+        this.banner = document.getElementById(".banner");
+        this.btnNoticia = "";
     }
-  }
 
-  // Procura no Json de jogadores as informações do elenco
-  async elenco() {
+    async run() {
+        await this.noticia();
+        await this.elenco();
+        await this.rankClassificacao();
+        this.clicando();
+    }
 
-    try {
-      const response = await fetch(this.urlListJogador);
-      const json = await response.json();
+    async ordenaNoticia(json) {
+        // Ordena pela classificação
+        json.sort((a, b) => {
+            if (a.idNoticia > b.idNoticia) {
+                return -1;
+            }
+            if (a.idNoticia < b.idNoticia) {
+                return 1;
+            }
+            // console.log()
+        });
+    }
 
-      json.elenco.map((jogador) => {
+    // Procura no json de noticia, e informa os dados do jogo
+    async noticia() {
 
-        let texto = `
+        try {
+            const response = await fetch(this.urlNoticia);
+            const json = await response.json();
+            let noticia = json.jogo;
+            console.log(noticia)
+            for (let i = 0; i < noticia.length; i++) {
+                this.ordenaNoticia(noticia)
+
+                let header = noticia[i].titulo;
+                let paragrafo = noticia[i].noticia1;
+                let data = noticia[i].data;
+
+                var divNoticia = document.createElement("div");
+                var img = document.createElement("img");
+                var pData = document.createElement("p");
+                var pParagrafo = document.createElement("p");
+                var h2 = document.createElement("h2");
+                var a = document.createElement("a");
+
+                img.textContent = noticia[i].img;;
+                pData.textContent = data;
+                h2.textContent = header;
+                pParagrafo.textContent = paragrafo.substring(0, 250) + "...";
+                a.textContent = `Saiba mais...`;
+
+                divNoticia.appendChild(img);
+                divNoticia.appendChild(pData);
+                divNoticia.appendChild(h2);
+                divNoticia.appendChild(pParagrafo);
+                divNoticia.appendChild(a);
+
+                img.setAttribute("src", noticia[i].img);
+                divNoticia.classList.add("noticia");
+                divNoticia.classList.add("w33");
+                divNoticia.classList.add(data);
+                a.classList.add("btn_noticia");
+                // a.setAttribute("href", "noticia-single.html");
+                this.paragrafo.appendChild(divNoticia)
+                a.setAttribute("onclick", "futbah.clicando()");
+                a.classList.add(data);
+                if (i >= 2) { break }
+            }
+
+        } catch (error) {
+            // console.log(error)
+        }
+    }
+
+    // Procura no Json de jogadores as informações do elenco
+    async elenco() {
+
+        try {
+            const response = await fetch(this.urlListJogador);
+            const json = await response.json();
+
+            json.elenco.map((jogador) => {
+
+                let texto = `
           <div class="jogador">
           <div class="btn-avancar-jogador">
             <!-- ${jogador.id} -->
@@ -87,54 +99,54 @@ class Futbah {
             <p>Posição: ${jogador.posicao}</p>
             <p>Número: ${jogador.numero}</p>`
 
-        if (jogador.campeao.class == "campeao") {
-          for (let i = 0; i < jogador.campeao.qtd; i++) {
-            texto += `<img class="${jogador.campeao.class}"  src="images/estrela.png" alt="Foto do ${jogador.nome}" title="Campeão em ${jogador.campeao.ano}">`
-          }
+                if (jogador.campeao.class == "campeao") {
+                    for (let i = 0; i < jogador.campeao.qtd; i++) {
+                        texto += `<img class="${jogador.campeao.class}"  src="images/estrela.png" alt="Foto do ${jogador.nome}" title="Campeão em ${jogador.campeao.ano}">`
+                    }
+                }
+
+                texto +=
+                    `</div></div>`
+                this.elencoJogadores.innerHTML += texto;
+
+            })
+        } catch (err) {
+            // console.error(err);
         }
-
-        texto +=
-          `</div></div>`
-        this.elencoJogadores.innerHTML += texto;
-
-      })
-    } catch (err) {
-      // console.error(err);
     }
-  }
 
-  // Procura no Json de jogadores as informações de classificação
-  async rankClassificacao() {
+    // Procura no Json de jogadores as informações de classificação
+    async rankClassificacao() {
 
-    try {
-      let classificacao = "";
-      const response = await fetch(this.urlListJogador);
-      const json = await response.json();
+        try {
+            let classificacao = "";
+            const response = await fetch(this.urlListJogador);
+            const json = await response.json();
 
-      // Ordena pela classificação
-      json.elenco.sort((a, b) => {
-        if (a.classificacao > b.classificacao) {
-          return -1;
-        }
-        if (a.classificacao < b.classificacao) {
-          return 1;
-        }
-        // console.log()
-      });
+            // Ordena pela classificação
+            json.elenco.sort((a, b) => {
+                if (a.classificacao > b.classificacao) {
+                    return -1;
+                }
+                if (a.classificacao < b.classificacao) {
+                    return 1;
+                }
+                // console.log()
+            });
 
-      var i = 1;
-      json.elenco.map((jogador) => {
-        classificacao = `
+            var i = 1;
+            json.elenco.map((jogador) => {
+                classificacao = `
           <tbody> `
-        if (i < 5) {
-          classificacao += `<tr class="promoted">`
-        } else if (i > 17) {
-          classificacao += `<tr class="relegated">`
-        } else {
-          classificacao += `<tr>`
-        }
+                if (i < 5) {
+                    classificacao += `<tr class="promoted">`
+                } else if (i > 17) {
+                    classificacao += `<tr class="relegated">`
+                } else {
+                    classificacao += `<tr>`
+                }
 
-        classificacao += `
+                classificacao += `
                   <td>${i}</td>
                   <td>${jogador.nome}</td>
                   <td>${jogador.classificacao}</td>
@@ -143,70 +155,48 @@ class Futbah {
               </tr>
           <tbody>
           `
-        document.getElementById("classificacao").innerHTML += classificacao;
-        i++;
+                document.getElementById("classificacao").innerHTML += classificacao;
+                i++;
 
-      })
+            })
+            console.log(json.elenco)
+            localStorage.setItem('elenco', JSON.stringify(json.elenco));
 
-    } catch (err) {
-      // console.error(err);
-    }
-  }
-
-
-  async clicando() {
-    const response = await fetch(this.urlNoticia);
-    const json = await response.json();
-
-    this.btnNoticia = document.querySelectorAll('.noticia .w33')
-    // console.log(noticias);
-
-    NodeList.prototype.addEventListener = function (event, func) {
-      this.forEach(function (content, item) {
-        // console.log(item);
-        content.addEventListener(event, func);
-      });
-    }
-
-    this.btnNoticia.addEventListener("click", function (e) {
-      // console.log(this, "  awas clicked");
-      // console.log(json);
-
-      for (let i = 0; i < json.jogo.length; i++) {
-        if(e.target.classList[1] == json.jogo[i].data){
-          // console.log(json.jogo[i]);
-          debugger;
-          localStorage.setItem('noticia', JSON.stringify(json.jogo[i]));
-
-          window.location.href = "noticia-single.html";
-
-          // futbah.enviaNoticia(noticia);
+        } catch (err) {
+            // console.error(err);
         }
-      }
-      
-    });
-  }
+    }
 
-   // Procura no json de noticia, e informa os dados do jogo
-  // async enviaNoticia(noticia) {
+    async clicando() {
+        const response = await fetch(this.urlNoticia);
+        const json = await response.json();
 
-  //   try {
-      
-  //     noticiasSingle =
-  //     `<h2>${noticia.titulo}</h2>`
+        this.btnNoticia = document.querySelectorAll('.noticia .w33')
+        // console.log(noticias);
 
-  //     document.getElementById("noticia_single").innerHTML += noticiasSingle;
-  //     connsole.log(noticia)
-  //     // debugger
+        NodeList.prototype.addEventListener = function (event, func) {
+            this.forEach(function (content, item) {
+                // console.log(item);
+                content.addEventListener(event, func);
+            });
+        }
 
-      
+        this.btnNoticia.addEventListener("click", function (e) {
+            // console.log(this, "  awas clicked");
+            // console.log(json);
 
-  //   } catch (error) {
-  //     // console.log(error)
-  //   }
-  // }
+            for (let i = 0; i < json.jogo.length; i++) {
+                if (e.target.classList[1] == json.jogo[i].data) {
+                    localStorage.setItem('noticia', JSON.stringify(json.jogo[i]));
 
+                    window.location.href = "noticia-single.html";
 
+                    // futbah.enviaNoticia(noticia);
+                }
+            }
+
+        });
+    }
 }
 
 let futbah = new Futbah();
--- script/scriptSingle.js
+++ script/scriptSingle.js
@@ -1,36 +1,65 @@
 class Noticias {
     constructor() {
-        this.urlListJogador = 'jogadroes/list_jogador.json';
-        this.urlNoticia = 'jogadroes/list_noticia.json';
+        // this.urlListJogador = 'jogadroes/list_jogador.json';
+        // this.urlNoticia = 'jogadroes/list_noticia.json';
         this.banner = document.getElementById("noticia_single");
         this.noticiaString = localStorage.getItem('noticia'); // Receber a string
         this.noticia = JSON.parse(this.noticiaString); // transformar em objeto novamente
-        console.log(this.noticia);
+        this.jsonJogadores = localStorage.getItem('elenco'); // Receber a string
+        this.elenco = JSON.parse(this.jsonJogadores); // transformar em objeto novamente
+        // console.log(this.noticia);
     }
 
     async run() {
         await this.montaNoticiaSingle();
     }
 
+    async criarElemento(tag, atributos, pai) {
+        var elemento = document.createElement(tag);
+        for (var chave in atributos) {
+            elemento.setAttribute(chave, atributos[chave]);
+        }
+        if (pai) {
+            pai.appendChild(elemento);
+        }
+
+        return elemento;
+    }
+
 
     // Procura no json de noticia, e informa os dados do jogo
     async montaNoticiaSingle() {
         try {
+            var container = document.getElementById('noticia_single')
+            var div = await this.criarElemento("div", { class: "noticia w100" }, container);
+            var titulo = await this.criarElemento("h2", { class: "texto" }, div);
+            this.criarElemento("img", { src: this.noticia.img }, div);
+            var pData = await this.criarElemento("p", { class: "texto" }, div);
+            var h2Titulo = await this.criarElemento("h2", { class: "texto" }, div);
+            var pNoticia1 = await this.criarElemento("p", { class: "texto" }, div);
+            await this.criarElemento("br", { class: "texto" }, div);
+            var pNoticia2 = await this.criarElemento("p", { class: "texto" }, div);
+            await this.criarElemento("br", { class: "texto" }, div);
+            var pNoticia3 = await this.criarElemento("p", { class: "texto" }, div);
+            await this.criarElemento("br", { class: "texto" }, div);
 
-            let noticiasSingle =
-                `<h2>Resumo da partida... </h2>
-                <div class="noticia w100">
-                    <p class="texto">
-                        <img src="${this.noticia.img}" alt="">
-                    <p>${this.noticia.data}</p>
-                    <h2>${this.noticia.titulo}</h2>
-                    <p>${this.noticia.noticia1}</p>
-                    <br>
-                    <p>${this.noticia.noticia2}</p>
-                    <br>
-                    <p>${this.noticia.noticia3}</p>
-                </div>
-                `
+            var noticiasSingle = "";
+            if (this.noticia.teveJogo) {
+                titulo.textContent = "Resumo da partida...";
+                pData.textContent = this.noticia.data;
+                h2Titulo.textContent = this.noticia.titulo
+                pNoticia1.textContent = this.noticia.noticia1
+                pNoticia2.textContent = this.noticia.noticia2
+                pNoticia3.textContent = this.noticia.noticia3
+            } else if (this.noticia.teveJogo == false) {
+                titulo.textContent = "Resumo da partida...";
+                pData.textContent = this.noticia.data;
+                h2Titulo.textContent = this.noticia.titulo
+                pNoticia1.textContent = this.noticia.noticia1
+
+            }
+
+
 
             this.banner.innerHTML += noticiasSingle;
 
